802c9a65a57ad9e072e0a88f55f4de6c6a1f53ee
Akinori Toma <akinoritoma@appletoma.home>
2020-12-09 06:05:16 -0500

    implement category function

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import Category
 
 class PostForm(forms.Form):
+  category_data =  Category.objects.all()
+  category_choise = {}
+  for category in category_data:
+    category_choise[category] = category
+    
   title = forms.CharField(max_length=30, label= 'Title')
+  category = forms.ChoiceField(label='Category', widget=forms.Select, choices=list(category_choise.items()))
   content =  forms.CharField(label='Contents', widget= forms.Textarea())
   image = forms.ImageField(label='image', required=False)
--- app/models.py
+++ app/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+class Category(models.Model):
+  name =  models.CharField('Category', max_length=100)
+
+  def __str__(self):
+    return self.name
+
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.PROTECT)
   title = models.CharField("Title", max_length=200)
   image = models.ImageField(upload_to='images', verbose_name='image', null=True, blank=True)
   content = models.TextField("Contents")
