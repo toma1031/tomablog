bc736d9c5bef11ce02f1850ba128ba1ed573ae5d
Akinori Toma <akinoritoma@appletoma.home>
2020-12-24 04:39:12 -0500

    moved secret key to .env from settings.pi

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oy_8nw4z&i7&_iiwos0%jd%qmth$siwb*#(1#h*^)np618nir&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
